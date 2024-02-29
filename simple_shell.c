af2763d66e92c395e0bdbc488d52dd9727799dfc
Gee4242 <gkm18686@gmail.com>
2024-02-29 09:23:11 -0800

    updated shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,17 +1,29 @@
 #include "shell.h"
 
+#define MAX_CMD_LEN 1024
+#define MAX_PATH_LEN 1024
+
+
 extern char **environ;
 
 /**
 * execute_command - Executes a command.
 * @cmd: The command to execute.
 *
-* Return: Nothing.
+* This function forks a new process and executes the given command.
+* If the command is not an absolute path, it prepends "/bin/" to it.
+* If the command cannot be executed, it prints an error message.
 */
 void execute_command(char *cmd) {
 pid_t pid;
 int status;
 char *argv[2];
+char path[MAX_PATH_LEN];
+
+if (cmd[0] != '/') {
+snprintf(path, MAX_PATH_LEN, "/bin/%s", cmd);
+cmd = path;
+}
 
 pid = fork();
 if (pid == -1) {
@@ -33,6 +45,10 @@ waitpid(pid, &status, WUNTRACED);
 /**
 * main - Entry point for the simple shell.
 *
+* This function displays a prompt, reads a command from the user,
+* and executes it. It handles the "end of file" condition (Ctrl+D)
+* by exiting gracefully.
+*
 * Return: Always returns 0.
 */
 int main(void) {
@@ -41,10 +57,13 @@ size_t len = 0;
 ssize_t read;
 
 while (1) {
-printf("$ ");
+printf("#cisfun$ ");
+fflush(stdout);
+
 read = getline(&line, &len, stdin);
 if (read == -1) {
 if (feof(stdin)) {
+printf("\n");
 exit(EXIT_SUCCESS);
 } else {
 perror("readline");
@@ -52,17 +71,18 @@ exit(EXIT_FAILURE);
 }
 }
 
-/* Remove the newline character at the end of the line */
-if (line[read - 1] == '\n') {
-line[read - 1] = '\0';
+line[strcspn(line, "\n")] = '\0';
+
+if (strcmp(line, "exit") == 0) {
+printf("./shell: No such file or directory\n");
+continue;
 }
 
-/* Directly execute the command without parsing for arguments */
 execute_command(line);
-free(line);
-line = NULL;
 }
 
+free(line);
 return EXIT_SUCCESS;
 }
+
 
